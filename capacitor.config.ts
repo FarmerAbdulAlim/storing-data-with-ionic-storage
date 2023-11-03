4fe8597214911ff48f381d7a9e99e42a7f650f55
FarmerAbdulAlim <farmerabdulalim@gmail.com>
2023-11-03 09:42:57 +0600

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'storing-data-with-ionic-storage',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
